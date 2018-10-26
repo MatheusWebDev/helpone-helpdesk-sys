281fb2d30fbcf627db236f1be9d4b3b53baffdc9
Matheus Fernandes <matheussky.mfj@gmail.com>
2018-10-26 01:17:54 -0300

    Criado estrutura de um login ficticio

--- helpone-helpdesk-sys/Controllers/UsuarioController.cs
+++ helpone-helpdesk-sys/Controllers/UsuarioController.cs
@@ -1,129 +1,22 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
-using helpone_helpdesk_sys.DAL;
-using helpone_helpdesk_sys.Models;
+﻿using System.Web.Mvc;
 
 namespace helpone_helpdesk_sys.Controllers
 {
 	public class UsuarioController : Controller
-    {
-        private HelpDeskContext db = new HelpDeskContext();
+	{
+		//private HelpDeskContext db = new HelpDeskContext();
 
-        // GET: Usuario
-        public ActionResult Index()
-        {
-            var usuarios = db.Usuarios.Include(u => u.Equipe);
-            return View(db.Usuarios.ToList());
-        }
+		//GET: Fomulário Login
+		public ActionResult Login()
+		{
+			return View();
+		}
 
-        // GET: Usuario/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
+		//POST: Logar usuário
+		public ActionResult Logar()
+		{
+			return RedirectToAction("Index", "Home");
+		}
 
-        // GET: Usuario/Create
-        public ActionResult Create()
-        {
-            ViewBag.EquipeId = new SelectList(db.Equipes, "Id", "OMSituada");
-            return View();
-        }
-
-        // POST: Usuario/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Login,Senha,Daltonismo,TipoAcesso,EquipeId")] Usuario usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Usuarios.Add(usuario);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.EquipeId = new SelectList(db.Equipes, "Id", "OMSituada", usuario.EquipeId);
-            return View(usuario);
-        }
-
-        // GET: Usuario/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.EquipeId = new SelectList(db.Equipes, "Id", "OMSituada", usuario.EquipeId);
-            return View(usuario);
-        }
-
-        // POST: Usuario/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Login,Senha,Daltonismo,TipoAcesso,EquipeId")] Usuario usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(usuario).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.EquipeId = new SelectList(db.Equipes, "Id", "OMSituada", usuario.EquipeId);
-            return View(usuario);
-        }
-
-        // GET: Usuario/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
-            {
-                return HttpNotFound();
-            }
-            return View(usuario);
-        }
-
-        // POST: Usuario/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Usuario usuario = db.Usuarios.Find(id);
-            db.Usuarios.Remove(usuario);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+	}
 }
