19dc41a99d8065ff1edae81ee80fd21e6cebae84
Matheus Fernandes <matheussky.mfj@gmail.com>
2018-11-02 01:06:46 -0300

    Mantendo o LOGIN e ID do Usuario em uma SESSION

--- helpone-helpdesk-sys/Controllers/HomeController.cs
+++ helpone-helpdesk-sys/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using helpone_helpdesk_sys.DAL;
 using helpone_helpdesk_sys.Models;
 using helpone_helpdesk_sys.Models.Enums;
-using System.Web;
 using System.Web.Mvc;
 
 namespace helpone_helpdesk_sys.Controllers
@@ -12,14 +11,37 @@ namespace helpone_helpdesk_sys.Controllers
 
 		public ActionResult Index(Usuario usuarioLogado)
 		{
+
 			if (usuarioLogado.TipoAcesso == EnumTipoUsuario.Operador)
 			{
-				return View("IndexOp", usuarioLogado);
+				if (Session["userLogged"] != null && Session["userLoggedId"] != null)
+				{
+					Session["userLogged"] = null;
+					Session["userLoggedId"] = null;
+				}
+				return View("IndexOpe", usuarioLogado);
 			} else if (usuarioLogado.TipoAcesso == EnumTipoUsuario.Suporte || usuarioLogado.TipoAcesso == EnumTipoUsuario.Desenvolvimento)
 			{
+				if (Session["userLogged"] != null && Session["userLoggedId"] != null)
+				{
+					Session["userLogged"] = null;
+					Session["userLoggedId"] = null;
+				}
 				return View("IndexSup", usuarioLogado);
 			}
-			return View("IndexOp");
+			return View("IndexOpe");
+		}
+
+		[ChildActionOnly]
+		public PartialViewResult UserLogged(Usuario usuarioLogado)
+		{
+			if (Session["userLogged"] == null && Session["userLoggedId"] == null)
+			{
+				Session["userLogged"] = usuarioLogado.Login;
+				Session["userLoggedId"] = usuarioLogado.Id;
+			}
+
+			return PartialView("_UserLogged");
 		}
 	}
 }
