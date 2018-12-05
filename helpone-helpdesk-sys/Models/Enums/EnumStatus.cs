c512ab118f69ba66e2a67054694f8e113bd7be17
Matheus Fernandes <matheussky.mfj@gmail.com>
2018-12-05 16:50:21 -0200

    [MOD] Add descrição na Model EnumStatus

--- helpone-helpdesk-sys/Models/Enums/EnumStatus.cs
+++ helpone-helpdesk-sys/Models/Enums/EnumStatus.cs
@@ -1,12 +1,20 @@
-﻿namespace helpone_helpdesk_sys.Models.Enums
+﻿using System.ComponentModel;
+
+namespace helpone_helpdesk_sys.Models.Enums
 {
 	public enum EnumStatus
 	{
+		[Description("Aguardando Resposta")]
 		AguardandoResposta = 1,
+		[Description("Chamado Respondido")]
 		ChamadoRespondido = 2,
+		[Description("Cancelado")]
 		Cancelado = 3,
+		[Description("Finalizado sem Feedback")]
 		FinalizadoSemFeedback = 4,
+		[Description("Finalizado c/ Feedback +")]
 		FinalizadoFeedbackPositivo = 5,
+		[Description("Finalizado c/ Feedback -")]
 		FinalizadoFeedbackNegativo = 6
 	}
 }
